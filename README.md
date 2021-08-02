1c2313073c224a521de8b15df60a818e3b31922e
JinhuaSu <994485784@qq.com>
2021-08-02 13:31:19 +0000

    test the client especially api 5

---
+++ README.md
@@ -0,0 +1,10 @@
+# fastapi for abd
+
+## quick start
+
+```
+cd /data1/su/haoyu/building_damage_kd
+python 
+uvicorn fastapi_server:app
+python fastapi_client.py
+```
--- fastapi_client.py
+++ fastapi_client.py
@@ -3,7 +3,11 @@ import requests
 # f = {"myFile": open("/data1/su/pdd/afastapi/3_post.png", "rb")}
 req = requests.get("http://127.0.0.1:8000/check_download_result?file_name=3_result.png")
 print(req.text)
-# TODO(pandi): decode into picture format and save it
+print("api 6 done")
 
-
-# NOTE(pandi): previous api code(django): /data1/su/app/xview2/xviewapi/xviewapi/views.py
+# test for api 5
+req = requests.get(
+    "http://127.0.0.1:8000/cls_for_upload?pre_file_name=3_pre.png&post_file_name=3_post.png"
+)
+print(req.text)
+print("api 5 done")
